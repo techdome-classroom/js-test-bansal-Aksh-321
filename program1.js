49d11a023ff39c541449a419c7b2ed5a8a7fc1fe
Akshat Samadhiya <129150704+Aksh-321@users.noreply.github.com>
2024-02-23 07:56:19 +0000

    Auto commit on save

--- program1.js
+++ program1.js
@@ -8,4 +8,20 @@ var isValid = function(s) {
 
 module.exports = { isValid };
 
+function isValid(s) {
+    const stack = [];
+    const mapping = { ")": "(", "}": "{", "]": "[" };
 
+    for (let char of s) {
+        if (char in mapping) {
+            const topElement = stack.pop() || '#';
+            if (mapping[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
